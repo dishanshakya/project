aa11917b3d94dfdee315db362d185bec3624242d
dishaa <dishanshakya@gmail.com>
2024-06-20 19:33:56 +0545

    added item.js

---
+++ src/item.js
@@ -0,0 +1,38 @@
+function ItemView() {
+    return (
+        <div id="ItemView">
+
+        </div>
+    );
+}
+
+function Description() {
+    return (
+        <div id="description">
+
+        </div>
+    )
+}
+
+function Comments() {
+    return (
+        <div id="comments">
+
+        </div>
+    )
+}
+
+function SimilarItems() {
+    return (
+        <div id="similarItems">
+
+        </div>
+    )
+}
+function OrderOwner() {
+    return (
+        <div id="orderOwner">
+
+        </div>
+    )
+}
